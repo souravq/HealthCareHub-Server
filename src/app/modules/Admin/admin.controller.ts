706f46b647faf21db953e061b36f9ab33f0757cc
souravq <souravbera515@gmail.com>
2024-03-19 12:37:50 +0530

    filter data using query parameter

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
 const getAllAdminDataFromDB = async (req: Request, res: Response) => {
+  console.log(req.query);
   try {
-    const result = await AdminService.getAllAdminDataFromDB();
+    const result = await AdminService.getAllAdminDataFromDB(req.query);
     res.status(200).json({
       success: true,
       message: "All Admin data fetched !!!",
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,25 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const getAllAdminDataFromDB = async () => {
-  const result = await prisma.admin.findMany();
+const getAllAdminDataFromDB = async (param: any) => {
+  const result = await prisma.admin.findMany({
+    where: {
+      OR: [
+        {
+          name: {
+            contains: param.searchTerm,
+            mode: "insensitive",
+          },
+        },
+        {
+          email: {
+            contains: param.searchTerm,
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   return result;
 };
 
