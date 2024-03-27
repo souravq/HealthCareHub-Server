f9f2e65044e27d1ce8ab073137038217f540a33f
souravq <souravbera515@gmail.com>
2024-03-27 18:00:32 +0530

    file upload done using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,11 +1,26 @@
 import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import auth from "../../middlewares/auth";
+import multer from "multer";
+import path from "path";
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.post(
   "/",
   auth("SUPER_ADMIN", "ADMIN", "DOCTOR"),
+  upload.single("file"),
   userController.createAdmin
 );
 
